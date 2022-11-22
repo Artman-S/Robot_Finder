72c7cd07883da11dca6aea96be6bdcdca4e8d7af
SEBASTIEN_ARTESI <SEBASTIEN.ARTESI@gmail.com>
2022-11-22 22:13:05 +0100

    Création component RobotCard + ajout de style (step 1, 2, 3)

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 import React from 'react';
 /** Import de la donnée */
+import Robots from './data/data';
+import RobotCard from './Components/RobotCard';
+
 import './App.css';
 
-import Robots from './data/data';
 
 function App() {
   console.log(Robots)
   return (
     <div className="App">
+      <div className='title'>
       <h1>Robot Finder</h1>
+      </div>
+      {Robots.map((Robot) =>( 
+      <RobotCard {...Robot}/>
+      ))}
     </div>
   );
 }
