26492ba248eeb490ea931a3af5ce1514dcbd8758
SEBASTIEN_ARTESI <SEBASTIEN.ARTESI@gmail.com>
2022-12-02 23:12:34 +0100

    Etape 6 création d'un input dans app.js avec un state en vu de la création du filtre

--- src/App.js
+++ src/App.js
@@ -2,20 +2,27 @@ import React from 'react';
 /** Import de la donnée */
 import Robots from './data/data';
 import RobotCard from './Components/RobotCard';
-import RobotAdress from './Components/RobotAdress';
+import { useState } from 'react';
 
 import './App.css';
 
 
 function App() {
   console.log(Robots[0])
-  
+  //je créer un state pour mon input de recherche avec des quotes vides
+  const [searchInput, setSearchInput] = useState('')
+  const searchItems = (searchValue)=> { setSearchInput(searchValue)}
   /*je map sur mon component <Robot /> pour afficher tout mon tableau */
   return (
     <div className="App">
+      {console.log(searchInput)}
       <div className='title'>
       <h1>Robot Finder</h1>
-      <input type="text" placeholder='search' />
+      <input 
+      type="text" 
+      placeholder='search'
+      onChange={(e) => searchItems(e.target.value)}
+      />
       </div> 
       {Robots.map((Robot) =>( 
       <RobotCard {...Robot}/>
